a5ff9d1dbae46f87fc914cae084f7940b9e31b15
zs <zs@163.com>
2019-02-28 20:12:18 +0800

    添加pim.xml文件

--- idea-git-test/src/main/java/com/itheima/test/HelloWorld.java
+++ idea-git-test/src/main/java/com/itheima/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.itheima.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world...111222");
-        System.out.println("Hello world...");
+        System.out.println("Hello world...555");
         System.out.println("Hello world...");
         System.out.println("Hello world...");
         System.out.println("Hello world...");
