d671efd7618b9863db12afda95179aade84f48e1
zs <zs@163.com>
2019-02-28 19:40:06 +0800

    添加HelloWorld.java文件

---
+++ idea-git-test/src/main/java/com/itheima/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world...");
+        System.out.println("Hello world...");
+        System.out.println("Hello world...");
+        System.out.println("Hello world...");
+        System.out.println("Hello world...");
+    }
+}
