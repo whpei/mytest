1c2a603349e347e46975d66e3abff7198bb315bb
zs <zs@163.com>
2019-02-28 20:28:39 +0800

    mytest修改HelloWorld.java文件

--- idea-git-test/src/main/java/com/itheima/test/HelloWorld.java
+++ idea-git-test/src/main/java/com/itheima/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world...111222");
         System.out.println("Hello world...555666");
-        System.out.println("Hello world...");
+        System.out.println("Hello world...999");
         System.out.println("Hello world...");
         System.out.println("Hello world...");
     }
